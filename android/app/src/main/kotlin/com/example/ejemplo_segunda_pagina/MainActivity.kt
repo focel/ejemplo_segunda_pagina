765fd6bdca369c1c5325f7750049d48ffbbf152f
focel <34137573+focel@users.noreply.github.com>
2023-08-11 16:46:34 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ejemplo_segunda_pagina/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ejemplo_segunda_pagina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:ejemplo_segunda_pagina/second_page';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+          child: Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
